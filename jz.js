25e8396961d5658e26ed52841e199b32881f18ab
Jenna Zeigen <jenna.zeigen@gmail.com>
2017-05-14 15:59:45 -0400

    add npm scripts

--- jz.js
+++ jz.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// browserify jz.js -o jz-bundle.js -t [ babelify --presets [ es2015 ] ] -t uglifyify
-
 let Triangler = require('./triangler.js');
 
 let triangler = new Triangler();
