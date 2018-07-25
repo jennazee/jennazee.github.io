76ed623d2bb421b825d9274a1c703e33b584bde8
Jenna Zeigen <jenna.zeigen@gmail.com>
2018-07-24 22:59:36 -0400

    Add eng mgr talk to list; make css even cooler

--- compile-bct.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-browserify basiccolorterminal.js -o bct-bundle.js -t [ babelify --presets [ es2015 ] ] -t uglifyify
-cp ./node_modules/huemanities/basiccolorterminal.html basiccolorterminal.html
-cp ./node_modules/huemanities/huemanity.css huemanity.css
--- compile-glitchrothko.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-browserify glitchrothko.js -o glitchrothko-bundle.js -t [ babelify --presets [ es2015 ] ] -t uglifyify
-cp ./node_modules/huemanities/glitchrothko.html glitchrothko.html
-cp ./node_modules/huemanities/huemanity.css huemanity.css
